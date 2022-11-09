1025916a64f675160ad36c636d1772f4b300e7c8
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-11-08 23:14:45 -0300

    REFACT: altera localStorage para API json-server

--- src/store/modules/itens/itens.js
+++ src/store/modules/itens/itens.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default {
     namespaced: true,
     state() {
@@ -6,52 +8,17 @@ export default {
             item: {}, // Item selecionado para edição
             toEdit: null, // Código do patrimônio
             stats: {}, // Cálculos para SMALL CARDS no inventário
-            saved: false
+            error: false
         }
     },
     mutations: {
-        // Salva um objeto item novo ou editado no localStorage
-        saveItem(state, item) {
-            // Caso localStorage vazio, cria um array com objeto item e salva
-            if (localStorage.getItem('itens') === null) {
-                let itens = []
-                itens.push(item)
-                localStorage.setItem('itens', JSON.stringify(itens))
-            } else { // Caso já exista um array objetos item
-                let list = JSON.parse(localStorage.getItem('itens'))
-                //No caso de item editado, apenas substitui o item
-                let already = false
-                list.forEach(prod => {
-                    // Verifica se ao menos 3 keys são iguais
-                    let matches = 0
-                    for (const [key,] of Object.entries(prod)) {
-                        if (prod[key] == item[key]) {
-                            matches++
-                            if (matches > 2) {
-                                break
-                            }
-                        }
-                    }
-                    if (matches > 2) {
-                        let index = list.indexOf(prod)
-                        list[index] = item
-                        state.saved = true
-                        already = true
-                    }
-                })
-                // Se não é um item editado, insere o novo objeto item no array
-                if (!already) {
-                    list.push(item)
-                    state.saved = true
-                }
-                // Salva o array objetos item atualizado no localStorage
-                localStorage.setItem('itens', JSON.stringify(list))
-            }
-        },
+        
         // Lista atual de todos os itens
         getItens(state) {
-            let allItens = JSON.parse(localStorage.getItem('itens'))
-            state.sendItens = allItens
+            axios.get("http://localhost:3000/itens")
+            .then((response) => {
+                state.sendItens = response.data;
+            })
         },
         // Deve-se chamar getItens primeiro
         // Seta state.item pelo código de patrimônio
@@ -99,7 +66,79 @@ export default {
                 }
             })
             state.stats.emprestados = emprestados  
+        },
+
+    },
+    actions: {
+        // Salva um objeto item novo ou editado no localStorage
+        async saveItem(context, item) {
+
+            await axios.get("http://localhost:3000/itens")
+            .then((response) => {
+                context.state.sendItens = response.data;
+            })
+            context.state.sendItens.forEach(element => {
+                if(element.patrimonio === item.patrimonio) {
+                    context.state.error = true
+                    return false
+                }
+            });
+            if(!context.state.error) {
+                
+                context.state.error = false
+                var headers = {
+                    "Content-Type": "application/json"
+                }
+                await axios.post("http://localhost:3000/itens", item, headers)
+                .then(() => {
+                    return true
+                })
+                .catch((e) => {
+                    console.error("error", e)
+                })
+                .finally(() => {
+                })
+            }
+            
         }
+
+
+    //         // Caso localStorage vazio, cria um array com objeto item e salva
+    //         if (localStorage.getItem('itens') === null) {
+    //             let itens = []
+    //             itens.push(item)
+    //             localStorage.setItem('itens', JSON.stringify(itens))
+    //         } else { // Caso já exista um array objetos item
+    //             let list = JSON.parse(localStorage.getItem('itens'))
+    //             //No caso de item editado, apenas substitui o item
+    //             let already = false
+    //             list.forEach(prod => {
+    //                 // Verifica se ao menos 3 keys são iguais
+    //                 let matches = 0
+    //                 for (const [key,] of Object.entries(prod)) {
+    //                     if (prod[key] == item[key]) {
+    //                         matches++
+    //                         if (matches > 2) {
+    //                             break
+    //                         }
+    //                     }
+    //                 }
+    //                 if (matches > 2) {
+    //                     let index = list.indexOf(prod)
+    //                     list[index] = item
+    //                     state.error = true
+    //                     already = true
+    //                 }
+    //             })
+    //             // Se não é um item editado, insere o novo objeto item no array
+    //             if (!already) {
+    //                 list.push(item)
+    //                 state.error = true
+    //             }
+    //             // Salva o array objetos item atualizado no localStorage
+    //             localStorage.setItem('itens', JSON.stringify(list))
+    //         }
+    //     },
     },
     getters: {
         // Retorna o último item selecionado para edição
