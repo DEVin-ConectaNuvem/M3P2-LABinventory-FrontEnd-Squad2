b459b3b3c7c2467d587d42a29d64e0ecae6942be
Adriano Matos Meier <adriano.meier@softplan.com.br>
2022-11-09 22:56:31 -0300

    C3 - Testes E2E - testes botoes menu lateral

---
+++ tests/e2e/specs/menu-lateral.js
@@ -0,0 +1,46 @@
+describe("Testa botoes do menu lateral", () => {
+    it("Clicar em botoes do menu lateral para validar componentes, rotas e o hide", () => {
+        const email = "adriano@email.com"
+        const password = "abcd1234"
+
+        // Login
+        cy.visit("/")
+        cy.get('#loginform > :nth-child(1) > .form-control').type(email)
+        cy.get('#loginform > :nth-child(2) > .form-control').type(`${password}{enter}`)
+        cy.contains("p", "Inventario")
+        
+        // Testa componente/rota botão de inventário
+        cy.get('.btns-div > :nth-child(1) > :nth-child(2)').click()
+        cy.contains("p", "Inventario")
+        cy.contains("h3", "Buscar itens")
+        
+        // Testa componente/rota botão de cadastro de colaboradores
+        cy.get('.btns-div > :nth-child(2) > :nth-child(2)').click()
+        cy.contains("p", "Cadastro de colaborador")
+        cy.contains("p", "Preencha os campos para cadastrar")
+        
+        // Testa componente/rota botão de listagem de colaboradores
+        cy.get('.btns-div > :nth-child(2) > :nth-child(3)').click()
+        cy.contains("p", "Colaboradores")
+        cy.contains("h3", "Buscar um colaborador")
+        
+        // Testa componente/rota do botão de cadastro de itens
+        cy.get('.btns-div > :nth-child(3) > :nth-child(2)').click()
+        cy.contains("p", "Cadastro de item")
+        cy.contains("p", "Preencha os campos para cadastrar")
+        
+        // Testa componente/rota do empréstimo de itens
+        cy.get('.btns-div > :nth-child(3) > :nth-child(3)').click()
+        cy.contains("p", "Emprestar")
+        cy.contains("h3", "Buscar por patrimônio")
+        
+        // Testa o botão de esconder o menu lateral
+        cy.get('#switchCircle').click()
+        cy.get(':nth-child(1) > :nth-child(1) > .onlyIcon > .fa-solid')
+        cy.get('#switchCircle').click()
+        
+        // Testa logout/botão de sair
+        cy.get('.btns-div > :nth-child(1) > :nth-child(3)').click()
+        cy.contains("h2", "Login")
+    })
+})
