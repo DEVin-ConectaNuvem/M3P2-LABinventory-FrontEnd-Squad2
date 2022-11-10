eb56c3143abc945f113a0725f3e1ae2dfc94e405
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-11-10 15:23:01 -0300

    teste e2e completo

---
+++ tests/e2e/specs/navbar.js
@@ -0,0 +1,37 @@
+// import {routes} from '../../../src/routes/routes/routes'
+// https://docs.cypress.io/api/table-of-contents
+// import { TemplateView } from '../../../src/views/TemplateView.vue'
+
+describe('Navbar tests', () => {
+    it('Check NavBar title, user name, and user icon', () => {
+        
+        const name = 'Marcoelho'
+        const email = 'marcoelho@email.com'
+        const password = '12345678'
+        
+        cy.visit('/')
+        cy.get('.criar-conta > .btn').click()
+        cy.get('#registerform > :nth-child(1) > .form-control').type(name)
+        cy.get('#registerform > :nth-child(2) > .form-control').type(email)
+        cy.get(':nth-child(3) > .form-control').type(password)
+        cy.get('.modal-footer > .btn-info').click()
+        cy.get('#loginform > :nth-child(1) > .form-control').type(email)
+        cy.get('#loginform > :nth-child(2) > .form-control').type(password)
+        cy.get('#loginform > .btn-info').click()
+        cy.location().should((loc) => {
+            expect(loc.toString()).to.eq('http://localhost:8080/#/users/inventario')
+        })
+        cy.get('.router').children().first().get('.current-route').should('have.text', 'Inventario')
+        cy.get('.router').children().first().get('#user').get('img.gravatar').should('be.visible')
+        cy.get('.router').children().first().get('#user').children().first().should('have.text', 'Marcoelho')
+        cy.get('.btns-div > :nth-child(2) > :nth-child(3)').click()
+        cy.get('.router').children().first().get('.current-route').should('have.text', 'Colaboradores')
+        cy.get('.btns-div > :nth-child(3) > :nth-child(3)').click()
+        cy.get('.router').children().first().get('.current-route').should('have.text', 'Emprestar')
+        cy.get('.btns-div > :nth-child(2) > :nth-child(2)').click()
+        cy.get('.router').children().first().get('.current-route').should('have.text', 'Cadastro de colaborador')
+        cy.get('.btns-div > :nth-child(3) > :nth-child(2)').click()
+        cy.get('.router').children().first().get('.current-route').should('have.text', 'Cadastro de item')
+      
+    })
+  })
