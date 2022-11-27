2111dd72abdd39d081a30efb0e09726780f190ad
juliamoura <55239787+juliasilvamoura@users.noreply.github.com>
2022-11-27 16:01:00 -0300

    teste inventory-item

--- tests/e2e/specs/item-inventory.js
+++ tests/e2e/specs/item-inventory.js
@@ -7,7 +7,7 @@ describe("Testa tela inventory book itens", () => {
         const url = "https://lojaibyte.vteximg.com.br/arquivos/ids/185030-1200-1200/39030-01-notebook-samsung-flash-f30-intel-n4000-4gb-ssd-64gb-13-3-full-hd-windows-10.jpg";
         const codigoDePatrimonio = "BR1234-789";
         const titulo = "Pc rosa";
-        const valorProduto = "3000,00";
+        const valorProduto = "3009,80";
         const marcaProduto = "Samsung";
         const modeloProduto = "PC-F30NP530XBB";
         const descricao = "Anyway";
@@ -47,7 +47,7 @@ describe("Testa tela inventory book itens", () => {
         const url2 = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs2.glbimg.com%2FEt0Z78geq2y9LpHy0U7e0OUHZWQ%3D%2F0x0%3A1200x630%2F984x0%2Fsmart%2Ffilters%3Astrip_icc()%2Fi.s3.glbimg.com%2Fv1%2FAUTH_08fbf48bc0524877943fe86e43087e7a%2Finternal_photos%2Fbs%2F2018%2F3%2FF%2FwN7K1ESaqCaKHclCcNAw%2Fwhatsapp-image-2018-11-26-at-20.19.38.jpeg&imgrefurl=https%3A%2F%2Fwww.techtudo.com.br%2Fnoticias%2F2018%2F11%2Fo-que-significa-pc-notebook-4k-mah-e-outros-termos-de-tecnologia.ghtml&tbnid=j_kAB9KVTu4ShM&vet=12ahUKEwiko_WKoaL7AhVqBbkGHcqxDk8QMygBegUIARDmAQ..i&docid=vMCMixvokim4iM&w=984&h=517&q=imagem%20pc&ved=2ahUKEwiko_WKoaL7AhVqBbkGHcqxDk8QMygBegUIARDmAQ";
         const codigoDePatrimonio2 = "XX1234-123";
         const titulo2 = "Pc gamer";
-        const valorProduto2 = "5000,00";
+        const valorProduto2 = "5008,99";
         const marcaProduto2 = "Dell";
         const modeloProduto2 = "PC-3000";
         const descricao2 = "Anyway";
@@ -77,7 +77,7 @@ describe("Testa tela inventory book itens", () => {
         // Conferir dados do produto
         cy.get('.btns-div > :nth-child(1) > :nth-child(2)').click();
         cy.get('#search-item').type("Pc gamer", {force: true})
-        cy.get('.img').click();
+        
     })
 
 
